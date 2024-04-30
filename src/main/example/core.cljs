c181002aa0a0d057b269aac5e75116004ca4cd62
Prakash Balodi <prakash.balodi@defnx.tech>
2024-04-30 13:43:54 +0530

    Update deps

--- src/main/example/core.cljs
+++ src/main/example/core.cljs
@@ -1,10 +1,10 @@
-(ns core
+(ns example.core
   (:require ["react-dom/client" :refer [createRoot]]
             [goog.dom :as gdom]
             [reagent.core :as r]))
 
 (defn app []
-      [:div "Hello world!"])
+      [:div "Hello world1!"])
 
 (defonce root (createRoot (js/document.getElementById "root")))
 
